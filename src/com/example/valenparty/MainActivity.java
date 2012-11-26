2870f7023a4532422a922432b7f8aed7b9b843db
Josanto <Josanto@158.42.76.230>
2012-11-26 13:07:46 +0100

    nueva modificacion
    Signed-off-by: Josanto <Josanto@158.42.76.230>

--- src/com/example/valenparty/MainActivity.java
+++ src/com/example/valenparty/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*Comentario de prueba POR MI*/
+        /*Comentario de prueba POR MI again*/
     }
 
     @Override
