31377ce4426b276ddf932a26e2574f6ea8da9fe1
Miguel Angel <mipegir@gmail.com>
2012-11-26 13:40:55 +0100

    comentando voy
    Signed-off-by: Miguel Angel <mipegir@gmail.com>

--- src/com/example/valenparty/MainActivity.java
+++ src/com/example/valenparty/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         /*Caca de la vaca 7*/
         /*Comentario de prueba POR MI again*/
+        /* COMENTARIO MIGUEL ANGEL*/
     }
 
     @Override
