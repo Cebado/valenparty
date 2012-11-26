07ee3231e4f73ad9f3664b0ae4ae081e4fa3eca8
GusLAN <tavolanza@gmail.com>
2012-11-26 13:32:25 +0100

    mas pruebas

--- src/com/example/valenparty/MainActivity.java
+++ src/com/example/valenparty/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*Caca de la vaca*/
+        /*Caca de la vaca 7*/
         /*Comentario de prueba POR MI again*/
     }
 
